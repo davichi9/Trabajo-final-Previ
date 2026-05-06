68ef0d6f16721c875218a2f1330055fdc2b88fd4
Adamdalus <adambenito17@gmail.com>
2026-05-06 19:39:51 +0300

    Ahora el formulario redirige a la pagina de resultado

--- src/Controller/PedidoController.php
+++ src/Controller/PedidoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PedidosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,9 +10,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PedidoController extends AbstractController
 {
-    #[Route('/pedido', name: 'app_pedido')]
-    public function consultar(Request $request): Response
+    #[Route('/pedido', name: 'app_pedido', methods: ['GET'])]
+    public function consultar(): Response
     {
         return $this->render('pedido/consultar.html.twig');
+    }
+
+    #[Route('/pedido/resultado', name: 'app_pedido_resultado', methods: ['POST'])]
+    public function resultado(Request $request, PedidosRepository $pedidosRepository): Response
+    {
+        $numeroPedido = $request->request->get('numeroPedido');
+        $telefono = $request->request->get('telefono');
+
+        $pedido = $pedidosRepository->findOneBy(['id' => $numeroPedido]);
+
+        if (!$pedido || $pedido->getCliente()->getTelefonoNumero() !== $telefono) {
+            $this->addFlash('error', 'No se encontró ningún pedido con ese número y teléfono.');
+            return $this->redirectToRoute('app_pedido');
+        }
+
+        return $this->redirectToRoute('app_pedido_resultado_show', ['id' => $pedido->getId()]);
+    }
+
+    #[Route('/pedido/resultado/{id}', name: 'app_pedido_resultado_show', methods: ['GET'])]
+    public function resultadoShow(int $id): Response
+    {
+        return $this->render('pedido/resultado.html.twig');
     }
 }
